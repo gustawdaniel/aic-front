a6296f0880cdf11467e760cf60846ba1bd89f9c3
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-04-01 14:43:45 +0800

    sorting implemented

--- pages/articles.vue
+++ pages/articles.vue
@@ -11,7 +11,8 @@ import {
   BookOpenIcon,
   ArrowUturnLeftIcon,
   CloudArrowUpIcon,
-  TrashIcon
+  TrashIcon,
+  ChevronDownIcon
 } from '@heroicons/vue/24/solid'
 import { Article, ArticleState } from "~/intefaces/Article";
 import { ucFirst } from "~/composables/ucfirst";
@@ -23,6 +24,20 @@ import dayjs from "dayjs";
 import { ClickableLink } from "~/intefaces/ClickableLink";
 import WordHighlighter from "vue-word-highlighter";
 
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue'
+
 const router = useRouter();
 
 const articles = useArticles()
@@ -33,6 +48,26 @@ const token = useToken();
 function setPage(link: ClickableLink) {
   loadArticles(link.url)
 }
+
+type SortType = 'id_desc' | 'id_asc' | 'title_asc';
+
+const sortValue = ref<SortType>('id_desc');
+
+const sortOptions = computed<Array<{ name: string, value: SortType, current: boolean }>>(() => {
+  const options: Array<{ name: string, value: SortType }> = [
+    {name: 'Newest', value: 'id_desc'},
+    {name: 'Oldest', value: 'id_asc'},
+    {name: 'Alphabetical', value: 'title_asc'},
+  ];
+
+  return options.map((v) => ({...v, current: v.value === sortValue.value}))
+});
+
+function setSort(value: SortType): void {
+  sortValue.value = value;
+  loadArticles(articlesUrl.value)
+}
+
 
 type ArticleGroupSingleResult = {
   _count: {
@@ -110,18 +145,24 @@ const tabs = computed(() => {
   }))
 })
 
+const articlesUrl = computed<string>(() => `/article?${ new URLSearchParams({
+  state: openTab.value,
+  search: search.value.text,
+  sort: sortValue.value
+}) }`);
+
 watch(search.value, (p, n) => {
   console.log("p,n", p, n);
   throttle(1000, (s: { enabled: boolean, text: string }) => {
     console.log('s:', s);
-    loadArticles(`/article?${ new URLSearchParams({state: openTab.value, search: s.text}) }`)
+    loadArticles(articlesUrl.value)
     loadArticleStats()
   })(n);
 })
 
 function setTab(id: string): void {
   openTab.value = id;
-  loadArticles(`/article?${ new URLSearchParams({state: openTab.value, search: search.value.text}) }`)
+  loadArticles(articlesUrl.value)
 }
 
 function readArticle(articleId: string) {
@@ -262,6 +303,40 @@ function getDateFromMongoId(id: string) {
                 <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">Date</th>
                 <th scope="col" class="relative py-3.5 pl-3 pr-6 sm:pr-0">
                   <span class="sr-only">Edit</span>
+
+
+                  <Menu as="div" class="relative inline-block text-left">
+                    <div>
+                      <MenuButton
+                          class="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+                        Sort
+                        <ChevronDownIcon
+                            class="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"/>
+                      </MenuButton>
+                    </div>
+
+                    <transition enter-active-class="transition ease-out duration-100"
+                                enter-from-class="transform opacity-0 scale-95"
+                                enter-to-class="transform opacity-100 scale-100"
+                                leave-active-class="transition ease-in duration-75"
+                                leave-from-class="transform opacity-100 scale-100"
+                                leave-to-class="transform opacity-0 scale-95">
+                      <MenuItems
+                          class="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div class="py-1">
+                          <MenuItem v-for="option in sortOptions" :key="option.name" v-slot="{ active }">
+                            <a @click="setSort(option.value)"
+                               :class="[option.current ? 'font-medium text-gray-900' : 'text-gray-500', active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm']">{{
+                                option.name
+                              }}</a>
+                          </MenuItem>
+                        </div>
+                      </MenuItems>
+                    </transition>
+                  </Menu>
+
+
                 </th>
               </tr>
               </thead>
@@ -273,7 +348,9 @@ function getDateFromMongoId(id: string) {
                       :href="article.request?.url" target="_blank"
                       class="text-ellipsis">
                     <WordHighlighter :query="search.text">
-                    {{ getArticleTitle(article).substring(0, 100).trim() + (getArticleTitle(article).length > 100 ? '...' : '') }}
+                      {{
+                        getArticleTitle(article).substring(0, 100).trim() + (getArticleTitle(article).length > 100 ? '...' : '')
+                      }}
                     </WordHighlighter>
                   </component>
                 </td>
